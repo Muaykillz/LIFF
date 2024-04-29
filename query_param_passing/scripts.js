6e6b4af7650d8713f740349d9cc248eb32d5badc
Muaykillz <muaykillz2548@gmail.com>
2024-04-29 21:30:48 +0700

    first commit query param passing

--- query_param_passing/scripts.js
+++ query_param_passing/scripts.js
@@ -10,12 +10,5 @@ console.log("Query String: ", queryString);
 
 async function main() {
     await liff.init({ liffId: "1660687033-5prxxNRm" })
-    if (liff.isLoggedIn()) {
-        console.log("LIFF is logged in.");
-    } else {
-        console.log("LIFF is not logged in.");
-        liff.login();
-        // { redirectUri: "https://liff-six.vercel.app/query_param_passing/"}
-    }
 }
 main()
